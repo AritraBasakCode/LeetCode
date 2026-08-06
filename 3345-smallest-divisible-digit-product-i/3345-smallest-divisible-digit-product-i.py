4f5e9f59a6a84c8ddbb03a725ef6b7cbf65b1c0c
Aritra Basak <104417333+AritraBasakCode@users.noreply.github.com>
2026-08-06 22:07:20 +0530

    Time: 0 ms (100%), Space: 19.3 MB (73.98%) - LeetHub

--- 3345-smallest-divisible-digit-product-i/3345-smallest-divisible-digit-product-i.py
+++ 3345-smallest-divisible-digit-product-i/3345-smallest-divisible-digit-product-i.py
@@ -1,13 +1,10 @@
 class Solution:
     def smallestNumber(self, n: int, t: int) -> int:
-        def check(num: int) -> bool:
-            pdt = 1
-            while num > 0:
-                pdt *= num % 10
-                num //= 10
-                if pdt == 0:
-                    break
-            return pdt % t == 0
-        while not check(n):
-            n += 1
-        return n
+        for i in range(n,n+1000):
+            x=i
+            pr=1
+            while x>0:
+                pr*=x%10
+                x//=10
+            if pr%t==0:
+                return i
